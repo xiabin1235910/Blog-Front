81ed8e77be2037d2a801ded2e09a5efa29cb4c92
xiabin1235910 <416854144@qq.com>
2016-08-22 21:43:29 +0800

    complete the front welcome page framework

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,8 @@
  * Created by Ben on 8/22/2016.
  */
 import { Component } from '@angular/core';
+declare var jQuery:any;
+
 @Component({
     selector: 'my-app',
     template: '<h1>My First Angular 2 App</h1>'
