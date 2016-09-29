9a1852d3c9a82abef633f7dbcca299271ff6ee9d
xiabin1235910 <416854144@qq.com>
2016-09-29 18:26:39 +0800

    complete little demo - 1

--- app/model/Tag.ts
+++ app/model/Tag.ts
@@ -5,4 +5,36 @@ export class Tag {
     id: number;
     createdate: string;
     name: string;
+
+    constructor(id?: number, name?:string, createdate?: string){
+        this.id = id;
+        this.name = name;
+        this.createdate = createdate;
+    }
+
+    constructor(_tag: Tag){
+        this.id = _tag.id;
+        this.name = _tag.name;
+        this.createdate = _tag.createdate;
+    }
+
+    setId(_id: number) {
+        this.id = _id;
+    }
+
+    setName(_name: string) {
+        this.name = _name;
+    }
+
+    setCreatedate(_createdate: string) {
+        this.createdate = _createdate;
+    }
+
+    setTag(_tag: Tag) {
+        if (_tag) {
+            this.setId(_tag.id);
+            this.setName(_tag.name);
+            this.setCreatedate(_tag.createdate);
+        }
+    }
 }
--- app/model/User.ts
+++ app/model/User.ts
@@ -5,4 +5,30 @@ export class User{
     id: number;
     name: string;
     createdate: string;
+
+    constructor(id?: number, name?:string, createdate?: string){
+        this.id = id;
+        this.name = name;
+        this.createdate = createdate;
+    }
+
+    setId(_id: number) {
+        this.id = _id;
+    }
+
+    setName(_name: string) {
+        this.name = _name;
+    }
+
+    setCreatedate(_createdate: string) {
+        this.createdate = _createdate;
+    }
+
+    setUser(_user: User) {
+        if(_user) {
+            this.setId(_user.id);
+            this.setName(_user.name);
+            this.setCreatedate(_user.createdate);
+        }
+    }
 }
