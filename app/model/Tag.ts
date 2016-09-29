4abc9d2f839da45809c4e133a7caba6e432a83ab
xiabin1235910 <416854144@qq.com>
2016-09-29 15:44:00 +0800

    blog model

---
+++ app/model/Tag.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by Ben on 9/27/2016.
+ */
+export class Tag {
+    id: number;
+    createdate: string;
+    name: string;
+}
---
+++ app/model/User.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by Ben on 9/22/2016.
+ */
+export class User{
+    id: number;
+    name: string;
+    createdate: string;
+}
